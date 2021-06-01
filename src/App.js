f5845a505f6ff33353cc757683be43f174a44291
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-06-01 14:38:46 +0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+import {Route,Switch} from 'react-router-dom'
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+        george
+        <Switch>
+          
+        </Switch>
+      </>
+    )
+  }
+}
+
+export default App;
